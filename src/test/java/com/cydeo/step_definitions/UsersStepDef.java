2a110787b188f78a3ee6ed079a7f289132fcfde5
adamar <aysun114@hotmail.com>
2024-07-03 11:50:48 -0500

    Page and Stepdef class added

--- src/test/java/com/cydeo/step_definitions/UsersStepDef.java
+++ src/test/java/com/cydeo/step_definitions/UsersStepDef.java
@@ -1,4 +1,38 @@
 package com.cydeo.step_definitions;
 
+import com.cydeo.pages.DashboardPage;
+import com.cydeo.pages.UsersPage;
+import com.cydeo.utilities.BrowserUtils;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
 public class UsersStepDef {
+
+    DashboardPage dashboardPage = new DashboardPage();
+
+    @When("there should be {int} users")
+    public void there_should_be_users(int expectedUserAmount) {
+
+        BrowserUtils.waitForVisibility(dashboardPage.usersAmount,15);
+
+      String expectedUserCount = String.valueOf(expectedUserAmount); // expectedUserAmount+""
+      String actualUserCount = dashboardPage.usersAmount.getText();
+
+        Assert.assertEquals(expectedUserCount,actualUserCount);
+    }
+
+    @When("I click on {string} link")
+    public void i_click_on_link(String string) {
+
+    }
+
+    @Then("table should have the following column names:")
+    public void table_should_have_the_following_column_names(io.cucumber.datatable.DataTable dataTable) {
+
+
+    }
+
+
+
 }
